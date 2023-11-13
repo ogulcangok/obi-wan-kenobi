ab572fadae56ea3416550e78bf31ae8e89a6c7f4
Oğulcan Gök <ogulcangok1@gmail.com>
2023-11-13 08:19:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify, make_response
 # Use a pipeline as a high-level helper
 from transformers import pipeline
+import logging
 
 food_classification = pipeline("image-classification", model="Kaludi/food-category-classification-v2.0")
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 @app.route("/classify", methods=["POST"])
@@ -16,6 +19,9 @@ def classify():
     body = request.get_json()
     url = body.get("url")
     food_class = food_classification(url)
+    resp = jsonify(food_class)
+    resp["url"] = url
+    logging.debug(resp)
     return make_response(jsonify(food_class), 200)
 
 
